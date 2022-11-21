df856bb1d17d32730d884f63439ca002d058466c
Roman Arenas <roman.ea3232@gmail.com>
2022-11-21 04:05:33 -0300

    3 commit (add protected route)

--- src/components/ProtectedRoutes.jsx
+++ src/components/ProtectedRoutes.jsx
@@ -1,16 +1,15 @@
-import { useSelector } from 'react-redux';
-import { Navigate, Outlet } from 'react-router-dom';
+import { useSelector } from "react-redux";
+import { Navigate, Outlet } from "react-router-dom";
 
 const ProtectedRoutes = () => {
-    const nameUser = useSelector(state => state.name)
-		// Aquí va la condición. Puede ser una condición de cualquier tipo. Lo que 
-		// Importa es que valide si el usuario está loggeado o no
-   // if(nameUser){
-    if(true){
-        return <Outlet />
-    } else { 
-        return <Navigate to='/' />
-    }                     // Aquí le debemos decir la ruta a la que queremos llevar
-};                        // al usuario si no está autenticado
+  const nameUser = useSelector((state) => state.name);
+  // Aquí va la condición. Puede ser una condición de cualquier tipo. Lo que
+  // Importa es que valide si el usuario está loggeado o no
+  if (nameUser) {
+    return <Outlet />;
+  } else {
+    return <Navigate to="/" />;
+  } // Aquí le debemos decir la ruta a la que queremos llevar
+}; // al usuario si no está autenticado
 
 export default ProtectedRoutes;
